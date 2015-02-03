76b49c9e56bdd1c8ca8e3cc5d577cb0825948d84
Marina <ohar0036@gmail.com>
2015-02-03 15:53:05 +0000

    Converted album view to Angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -35,6 +35,12 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
      templateUrl: '/templates/collection.html'
    });
    
+   $stateProvider.state('album', {
+     url: '/album',
+     templateUrl: '/templates/album.html',
+     controller: 'Album.controller'
+   });
+
  }]);
  
  blocJams.controller('Landing.controller', ['$scope', function($scope) {
@@ -72,3 +78,36 @@ blocJams.controller('Collection.controller', ['$scope', function($scope) {
       $scope.albums.push(angular.copy(albumPicasso));
     }
 }]);
+
+blocJams.controller('Album.controller', ['$scope', function($scope) {
+   $scope.album = angular.copy(albumPicasso);
+
+     var hoveredSong = null;
+     var playingSong = null;
+   
+     $scope.onHoverSong = function(song) {
+       hoveredSong = song;
+     };
+   
+     $scope.offHoverSong = function(song) {
+       hoveredSong = null;
+     };
+
+     $scope.getSongState = function(song) {
+       if (song === playingSong) {
+         return 'playing';
+       }
+       else if (song === hoveredSong) {
+         return 'hovered';
+       }
+       return 'default';
+     };
+
+      $scope.playSong = function(song) {
+        playingSong = song;
+      };
+   
+      $scope.pauseSong = function(song) {
+        playingSong = null;
+      };
+  }]);
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -300,6 +300,12 @@ blocJams.config(['$stateProvider', '$locationProvider', function($stateProvider,
      templateUrl: '/templates/collection.html'
    });
    
+   $stateProvider.state('album', {
+     url: '/album',
+     templateUrl: '/templates/album.html',
+     controller: 'Album.controller'
+   });
+
  }]);
  
  blocJams.controller('Landing.controller', ['$scope', function($scope) {
@@ -337,6 +343,39 @@ blocJams.controller('Collection.controller', ['$scope', function($scope) {
       $scope.albums.push(angular.copy(albumPicasso));
     }
 }]);
+
+blocJams.controller('Album.controller', ['$scope', function($scope) {
+   $scope.album = angular.copy(albumPicasso);
+
+     var hoveredSong = null;
+     var playingSong = null;
+   
+     $scope.onHoverSong = function(song) {
+       hoveredSong = song;
+     };
+   
+     $scope.offHoverSong = function(song) {
+       hoveredSong = null;
+     };
+
+     $scope.getSongState = function(song) {
+       if (song === playingSong) {
+         return 'playing';
+       }
+       else if (song === hoveredSong) {
+         return 'hovered';
+       }
+       return 'default';
+     };
+
+      $scope.playSong = function(song) {
+        playingSong = song;
+      };
+   
+      $scope.pauseSong = function(song) {
+        playingSong = null;
+      };
+  }]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
